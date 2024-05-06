910e1052dd4d85046e8f346aad84bbc2c422ffb9
Dave <shrps@posteo.net>
2024-05-06 14:40:42 +0200

    add simple transformer case

---
+++ tests/e2e/test_transformer_cases.py
@@ -0,0 +1,36 @@
+from typing import Dict, Any
+
+import pytest
+
+from tests.e2e.utils import get_source_from_open_api, get_dict_from_open_api
+from tests.cases import get_test_case_path
+
+
+@pytest.fixture(scope="module")
+def transformers() -> Dict[str, str]:
+    case_path = get_test_case_path("transformer_specs.yml")
+    # validate that source will work
+    get_source_from_open_api(case_path)
+
+    # get dict and save paginator info into dict
+    rendered_dict = get_dict_from_open_api(case_path)
+    return {
+        entry["name"]: entry  # type: ignore
+        for entry in rendered_dict["resources"]  # type: ignore
+    }
+
+
+def test_simple_transformer(transformers: Dict[str, Any]) -> None:
+    print(transformers)
+    assert transformers["collections"] == {
+        "name": "collections",
+        "endpoint": {"data_selector": "$", "path": "/collection/"},
+    }
+    assert transformers["single_collection"] == {
+        "name": "single_collection",
+        "endpoint": {
+            "data_selector": "$",
+            "path": "/collection/{collection_id}",
+            "params": {"collection_id": {"type": "resolve", "resource": "collections", "field": "id"}},
+        },
+    }

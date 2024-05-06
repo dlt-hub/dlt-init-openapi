d3ca9a23af934782d7190db253fd0eed0513491f
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-05-06 14:03:53 +0200

    Add observer api spec

---
+++ tests/e2e/test_observer_cases.py
@@ -0,0 +1,46 @@
+#
+# Test different iterations of observer spec: FastAPI generated
+#
+from tests.e2e.utils import get_dict_from_open_api, get_source_from_open_api
+from tests.cases import get_test_case_path
+
+
+def test_simple_observer_load() -> None:
+    api_spec = get_test_case_path("observer_with_pagination.yml")
+    source = get_dict_from_open_api(api_spec)
+    assert len(source["resources"]) == 2
+
+    assert source["resources"][0] == {
+        # FIXME: generates wierd resource name
+        "name": "admin_get_users_admin_users_get",
+        "endpoint": {
+            "data_selector": "items",
+            "path": "/admin/users",
+            "paginator": {
+                "type": "offset",
+                "initial_limit": "'100'",
+                "offset_param": "offset",
+                "limit_param": "limit",
+                "total_path": "total",
+            },
+        },
+    }
+    assert source["resources"][1] == {
+        # FIXME: generates wierd resource name
+        "name": "get_offices_offices_get",
+        "endpoint": {
+            "data_selector": "items",
+            "path": "/offices",
+            "paginator": {
+                "type": "offset",
+                "initial_limit": "'100'",
+                "offset_param": "offset",
+                "limit_param": "limit",
+                "total_path": "total",
+            },
+        },
+    }
+
+    # source should also work
+    dltsource = get_source_from_open_api(api_spec, base_url="https://api.opendota.com/api")
+    dltsource.resources["admin_get_users_admin_users_get"].add_limit(15)

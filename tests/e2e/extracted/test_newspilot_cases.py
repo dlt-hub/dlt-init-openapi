836e772deea715cf4c2af69267386c8166e73160
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-05-08 17:16:21 +0200

    Add newspilot

---
+++ tests/e2e/extracted/test_newspilot_cases.py
@@ -0,0 +1,22 @@
+#
+# Test different iterations of dotastats spec, this is FastAPI generated
+#
+import pytest
+
+from tests.e2e.utils import get_dict_by_case
+
+
+@pytest.mark.skip
+def test_teamgpt_load() -> None:
+    source = get_dict_by_case("extracted", "newspilot_with_pagination.yml")
+    assert len(source["resources"]) == 1
+
+    # FIXME: they use page and articles_per_page query parameters
+    assert source["resources"][0] == {
+        "name": "article",
+        "endpoint": {
+            "data_selector": "$",
+            "path": "/api/articles/",
+            "paginator": {},
+        },
+    }

03c4fed6da7ff8d04810beaa97ce024b651dd4f8
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-05-08 17:28:25 +0200

    Skip zoom case in all tests

--- tests/e2e/test_all_specs.py
+++ tests/e2e/test_all_specs.py
@@ -8,6 +8,10 @@ from tests.e2e.utils import get_all_spec_paths, get_source
     "case",
     get_all_spec_paths(),
 )
-def test_all_specs(case: str) -> None:
+@pytest.mark.parametrize("skip_case", ["zoom_with_pagination.yml"])
+def test_all_specs(case: str, skip_case: str) -> None:
+    if skip_case in case:
+        return
+
     source = get_source(case)
     assert len(source.resources) >= 1

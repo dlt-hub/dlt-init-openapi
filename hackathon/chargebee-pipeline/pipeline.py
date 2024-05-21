ae8740225bc6f70072c579e3e71886c3c73bc8ab
AstrakhantsevaAA <astra92293@gmail.com>
2024-05-21 17:36:01 +0200

    update

--- hackathon/chargebee-pipeline/pipeline.py
+++ hackathon/chargebee-pipeline/pipeline.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         pipeline_name="chargebee_pipeline",
         destination='duckdb',
         dataset_name="chargebee_data",
-        full_refresh=False,
+        full_refresh=True,
         export_schema_path="schemas/export"
     )
     source = chargebee_source()
